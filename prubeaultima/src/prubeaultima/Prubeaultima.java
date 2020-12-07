9f7582750bfc48a898b16cdb4c3250c56bddb955
hecyebesdelpino <61537808+hecyebesdelpino@users.noreply.github.com>
2020-12-07 14:04:29 +0100

    Add files via upload

---
+++ prubeaultima/src/prubeaultima/Prubeaultima.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prubeaultima;
+
+/**
+ *
+ * @author hecyebesdelpino
+ */
+public class Prubeaultima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola");
+        System.out.println("Que tal");
+        System.out.println("Adri");
+    }
+    
+}
